504555ec04ebe2601d06c049b25ec6b64feb4391
James Weston <jawone87@gmail.com>
2015-01-30 15:10:11 -0500

    FIRST

---
+++ server/index.js
@@ -0,0 +1,22 @@
+var Hapi = require('hapi');
+
+
+var server = new Hapi.Server();
+
+
+server.connection({
+    port: 7843
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+
+        return reply('Oh hai!');
+    }
+});
+
+
+server.start();
