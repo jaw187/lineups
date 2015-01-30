52c6ff5e610124e07b26c3e8c4d60a4294a48029
James Weston <jawone87@gmail.com>
2015-01-30 16:51:22 -0500

    Enable xframe

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ server.connection({
         security: {
             xframe: {
                 rule: 'allow-from',
-                source: 'thefantasyfix.com'
+                source: 'http://thefantasyfix.com'
             }
         }
     }
